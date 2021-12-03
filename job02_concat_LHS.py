eb25fd1bd304f6c9187761d4b73bbad66bbdc2ae
Hyeoun <ing02084@naver.com>
2021-12-03 12:38:25 +0900

    change concat code and add job03_one_sentence.py

--- job02_concat_LHS.py
+++ job02_concat_LHS.py
@@ -1,15 +1,27 @@
 import pandas as pd
 
-df = pd.DataFrame()
-years = 2017
-page = 53
-for i in range(1, page + 1):
-    df_temp = pd.read_csv('./crawling_data/reviews_{}_{}.csv'.format(years, i))
-    df_temp.dropna(inplace=True)
-    df_temp.drop_duplicates()
-    df_temp.columns = ['title', 'reviews']
-    df_temp.to_csv('./crawling_data/reviews_{}_{}.csv'.format(years, i), index=False)
-    df = pd.concat([df, df_temp], ignore_index=True)
+# df = pd.DataFrame()
+# years = 2017
+# page = 53
+# for i in range(1, page + 1):
+#     df_temp = pd.read_csv('./crawling_data/reviews_{}_{}.csv'.format(years, i))
+#     df_temp.dropna(inplace=True)
+#     df_temp.drop_duplicates(inplace=True)
+#     df_temp.columns = ['title', 'reviews']
+#     df_temp.to_csv('./crawling_data/reviews_{}_{}.csv'.format(years, i), index=False)
+#     df = pd.concat([df, df_temp], ignore_index=True)
+#
+# df.info()
+# df.to_csv('./crawling_data/reviews_{}.csv'.format(years), index=False)
 
+df = pd.DataFrame()
+for i in range(15, 22):
+    df_temp = pd.read_csv('./crawling_data/reviews_20{}.csv'.format(i))
+    df_temp.dropna(inplace=True)
+    df_temp.drop_duplicates(inplace=True)
+    df_temp.columns = ['title', 'reviews']
+    df_temp.to_csv('./crawling_data/reviews_20{}.csv'.format(i), index=False)
+    df = pd.concat([df, df_temp], ignore_index=True)
+df.drop_duplicates(inplace=True)
 df.info()
-df.to_csv('./crawling_data/reviews_{}.csv'.format(years), index=False)
+df.to_csv('./crawling_data/naver_movie_reviews_2015_2021.csv', index=False)
