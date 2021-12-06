b1ef170126f96f8e1ce785fac6b679371985130b
Hyeoun <ing02084@naver.com>
2021-12-06 11:42:03 +0900

    start visualization

---
+++ job05_word2vec_visualization.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from gensim.models import Word2Vec
+from sklearn.manifold import TSNE  # 차원축소
+from matplotlib import font_manager, rc  # 한글 사용시
+import matplotlib as mpl
